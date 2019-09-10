f6e46518894b2d68a2fcbce192e78d3b3ec9895e
Moon <justwe77@163.com>
2019-09-10 23:42:58 +0800

    失败的原因是因为包装类Integer的自动拆封装属性

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -2,6 +2,7 @@ package com.github.hcsp.multithread;
 
 public class Counter {
     private Integer value = 0;
+    private Object lock = new Object();
 
     public int getValue() {
         return value;
@@ -9,7 +10,7 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        synchronized (value) {
+        synchronized (lock) {
             value += i;
             return value;
         }
@@ -17,7 +18,7 @@ public class Counter {
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        synchronized (value) {
+        synchronized (lock) {
             value -= i;
             return value;
         }
