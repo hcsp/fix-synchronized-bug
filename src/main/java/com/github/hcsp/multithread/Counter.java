aba45c8f135c6e43e4b55c7c9f2bff3227887bd8
Hana <384073877@qq.com>
2019-09-26 23:10:30 +0800

    通过改变锁的对象解决

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -2,20 +2,26 @@ package com.github.hcsp.multithread;
 
 public class Counter {
     private Integer value = 0;
+    Object lock = new Object();
 
     public int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public synchronized int addAndGet(int i) {
+    public int addAndGet(int i) {
+        synchronized (lock) {
             value += i;
             return value;
+        }
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public synchronized int minusAndGet(int i) {
+    public int minusAndGet(int i) {
+        synchronized (lock) {
             value -= i;
             return value;
+        }
+
     }
 }
