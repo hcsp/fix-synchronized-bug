49aa9d432367a66a97b0a10fbdf3020955b5c0db
JiaXinWang61 <584937529@qq.com>
2019-11-05 12:20:46 +0800

    解决一个多线程的bug

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.multithread;
 
+
+
 public class Counter {
     private Integer value = 0;
+    private Object lock = new Object();
 
     public int getValue() {
         return value;
@@ -9,7 +12,7 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        synchronized (value) {
+        synchronized (lock) {
             value += i;
             return value;
         }
@@ -17,7 +20,7 @@ public class Counter {
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        synchronized (value) {
+        synchronized (lock) {
             value -= i;
             return value;
         }
