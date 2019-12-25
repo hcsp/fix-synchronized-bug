4eebdaf60bef3b817e1f9b9ae4f1d13938699cf8
ws1603 <18388866683@163.com>
2019-12-25 16:09:33 +0800

    解决一个多线程的bug

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,23 +1,24 @@
 package com.github.hcsp.multithread;
 
-public class Counter {
+class Counter {
     private Integer value = 0;
+    Object obj = new Object();
 
-    public int getValue() {
+    int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
-        synchronized (value) {
+    int addAndGet(int i) {
+        synchronized (obj) {
             value += i;
             return value;
         }
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
-        synchronized (value) {
+    int minusAndGet(int i) {
+        synchronized (obj) {
             value -= i;
             return value;
         }
