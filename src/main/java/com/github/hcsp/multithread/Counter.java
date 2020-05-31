3ba6f2f69da5d6425e3d898241d959a09b703ede
luhao <linesahao@outlook.com>
2020-05-31 16:25:20 +0800

    多线程中使用锁时，必须使用同一个锁。

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.multithread;
 
 public class Counter {
+    // 必须保证使用的是同一个锁，Integer随着加减引用一直在变化，两个synchronized拿到的不是同一个锁
+    private static final Object lock = new Object();
     private Integer value = 0;
 
     public int getValue() {
@@ -9,7 +11,7 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        synchronized (value) {
+        synchronized (lock) {
             value += i;
             return value;
         }
@@ -17,7 +19,7 @@ public class Counter {
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        synchronized (value) {
+        synchronized (lock) {
             value -= i;
             return value;
         }
