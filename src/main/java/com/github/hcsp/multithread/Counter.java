5eed0d3bd83ec7dc2293d34368f293fa50a51911
concise <15521608275@163.com>
2020-06-06 10:56:14 +0800

    try 2

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -2,6 +2,7 @@ package com.github.hcsp.multithread;
 
 public class Counter {
     private Integer value = 0;
+    private Object lock = new Object();
 
     public int getValue() {
         return value;
@@ -9,13 +10,18 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public synchronized int addAndGet(int i) {
+        synchronized (lock) {
             value += i;
             return value;
+        }
+
     }
 
     // 减去一个整数i，并返回减之后的结果
     public synchronized int minusAndGet(int i) {
+        synchronized (lock) {
             value -= i;
             return value;
+        }
     }
 }
