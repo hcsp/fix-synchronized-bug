abb635bc2e90775d6300a707a5a18ffbee640b54
McDul <McDul19960103@gmail.com>
2020-06-24 08:45:07 +0800

    创建一个可以被当作锁的对象

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -2,24 +2,23 @@ package com.github.hcsp.multithread;
 
 public class Counter {
     private Integer value = 0;
+    private static final Object LOCK = new Object();
 
     public int getValue() {
         return value;
     }
 
     // 加上一个整数i，并返回加之后的结果
-    public int addAndGet(int i) {
-        synchronized (value) {
+    public void addAndGet(int i) {
+        synchronized (LOCK) {
             value += i;
-            return value;
         }
     }
 
     // 减去一个整数i，并返回减之后的结果
-    public int minusAndGet(int i) {
-        synchronized (value) {
+    public void minusAndGet(int i) {
+        synchronized (LOCK) {
             value -= i;
-            return value;
         }
     }
 }
