c1695dafb850f8cac3a274e87a22457cf04cccbd
catmmao <catmmao@gmail.com>
2020-11-06 15:58:06 +0800

    add final lock (#128)
    add final lock (#128)

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.multithread;
 
 public class Counter {
+    private static final Object lock = new Object();
     private Integer value = 0;
 
     public int getValue() {
@@ -9,7 +10,7 @@ public class Counter {
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
-        synchronized (value) {
+        synchronized (lock) {
             value += i;
             return value;
         }
@@ -17,7 +18,7 @@ public class Counter {
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
-        synchronized (value) {
+        synchronized (lock) {
             value -= i;
             return value;
         }
