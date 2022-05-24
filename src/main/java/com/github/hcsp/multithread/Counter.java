f72d310d3e8978e65c9e2475c37c5918f3a464e4
Administrator <admin@example.com>
2022-05-24 20:11:04 +0800

    fix bug

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,25 +1,31 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
-    private Integer value = 0;
+//    private Integer value = 0;
+    private final AtomicInteger value = new AtomicInteger(0);
 
     public int getValue() {
-        return value;
+        return value.intValue();
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
         synchronized (value) {
-            value += i;
-            return value;
+            return value.addAndGet(i);
         }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
         synchronized (value) {
-            value -= i;
-            return value;
+
+            for (int j = 0; j < i; j++) {
+                value.decrementAndGet();
+            }
+
+            return value.intValue();
         }
     }
 }
