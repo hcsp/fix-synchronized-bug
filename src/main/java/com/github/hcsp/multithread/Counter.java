8768020fff2a8654158d1ecb99bd0fd74941fd61
SimonsYang <lynnyang@simons-mac.local>
2022-07-16 18:10:02 +0100

    using AtomicInteger replaced integer to make all operation atomic

--- src/main/java/com/github/hcsp/multithread/Counter.java
+++ src/main/java/com/github/hcsp/multithread/Counter.java
@@ -1,25 +1,26 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Counter {
-    private Integer value = 0;
+    private AtomicInteger value = new AtomicInteger(0);
 
     public int getValue() {
-        return value;
+        return value.get();
     }
 
     // 加上一个整数i，并返回加之后的结果
     public int addAndGet(int i) {
         synchronized (value) {
-            value += i;
-            return value;
+            return value.addAndGet(i);
+
         }
     }
 
     // 减去一个整数i，并返回减之后的结果
     public int minusAndGet(int i) {
         synchronized (value) {
-            value -= i;
-            return value;
+            return value.addAndGet(-i);
         }
     }
 }
